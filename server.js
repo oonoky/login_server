2263a5416b28acf32f61b98aa05738946bc3b676
Gulnaz Omirzakova <gulnaz.omirzakova@narxoz.kz>
2024-03-27 21:09:56 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+require('./config/db');
+
+const app = require('express')();
+const port = 3000;
+
+const UserRouter = require('./api/User');
+
+const bodyParser = require('express').json;
+app.use(bodyParser());
+
+app.use('/user', UserRouter)
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
